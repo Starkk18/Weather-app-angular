6d2deecd6869a8ba0e26b0af975f6ad95b0b18fb
Starkk18 <148677660+Starkk18@users.noreply.github.com>
2023-10-22 14:10:02 +0530

    Update environment.ts
    removing api key value

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@ export const environment = {
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
     XRapidAPIkeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIkeyHeaderValue: '7e230fa7aemsh81dd105994358edp18aed4jsn4a203a414ff2'
+    XRapidAPIkeyHeaderValue: 'check below commented line for value'
 
 
 };
+
+
+// line 7 value 7e230fa7aemsh81dd105994358edp18aed4jsn4a203a414ff2
